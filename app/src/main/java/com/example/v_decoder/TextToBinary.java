61845312003998fc0b429a85ebad5152887cd63a
CYBERCOTLETA <fluffy.siberian.cat@gmail.com>
2022-12-17 17:57:06 +1000

    just fixed animations

--- app/src/main/java/com/example/v_decoder/TextToBinary.java
+++ app/src/main/java/com/example/v_decoder/TextToBinary.java
@@ -17,7 +17,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 public class TextToBinary extends Activity {
     public EditText EditText;
     public TextView TextView;
