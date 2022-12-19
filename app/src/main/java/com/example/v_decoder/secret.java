1b636c5c87fd62cb5c5665bcfaf680e097f0446a
CYBERCOTLETA <fluffy.siberian.cat@gmail.com>
2022-12-19 13:47:25 +1000

    pre realise!

---
+++ app/src/main/java/com/example/v_decoder/secret.java
@@ -0,0 +1,15 @@
+package com.example.v_decoder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class secret extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secret);
+    }
+}
